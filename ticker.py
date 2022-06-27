1b0cafc5daaaa3f17f3e0c0e1bd24831e1a76cc7
ppipxam <p.peng@impaxam.com>
2022-06-27 12:46:37 +0800

    version 0.0.3

--- ticker.py
+++ ticker.py
@@ -5,11 +5,11 @@ from typing import Optional, Union, List, Dict, Callable
 from ._abstract import AbstractAPI
 from .utils.config import Config
 
-
+DEFAULT_CONFIG = "./FinancialModelingPrep/.config/config.json"
 class Ticker(AbstractAPI):
 
     def __init__(self, ticker: str, 
-        config: Union[str, Config]="./FinancialModelingPrep/.config/config.json", 
+        config: Union[str, Config]=DEFAULT_CONFIG, 
         mode='statements',
         **kwargs):
         super(Ticker, self).__init__(config=config,
@@ -18,18 +18,19 @@ class Ticker(AbstractAPI):
         if isinstance(ticker, str):
             if "," in ticker: 
                 tickers = ticker.upper().split(",")
+                tickers = [t.strip() for t in tickers]
                 assert len(tickers) == sum([t in self.available_tickers 
                     for t in tickers]), \
                     f"All tickers must be available! These are not valid tickers: {' '.join([t for t in tickers if t not in self.available_tickers])}"
-                self.tickers = tickers.upper()
+                self.tickers = tickers
             else:
                 assert ticker.upper().strip() in [t.upper() for t in self.available_tickers], "Not a valid ticker!"
                 self.tickers = [ticker.upper()]
         elif isinstance(ticker, list):
-            assert len(tickers) == sum([str(t).upper() in self.available_tickers 
+            assert len(tickers) == sum([str(t).upper().strip() in self.available_tickers 
                     for t in tickers]), \
                     f"All tickers must be available! These are not valid tickers: {' '.join([t for t in tickers if t not in self.available_tickers])}"
-            self.tickers = [str(t).upper for t in ticker]
+            self.tickers = [str(t).upper() for t in ticker]
 
     def __get_statements(self, statement='income', 
         freq="A", 
@@ -64,6 +65,7 @@ class Ticker(AbstractAPI):
                 'fillingDate', 'acceptedDate', 'calendarYear', 'period']).T
             df = df.stack(['symbol', 'cik']).swaplevel(0, 2)
             if save_to_sql:
+                assert self.sql_conn is not None, "sql_path must be specified if you want to use 'save_to_sql'"
                 start = f"{df.columns.get_level_values('calendarYear')[0]}\
                     {df.columns.get_level_values('period')[0]}"
                 end = f"{df.columns.get_level_values('calendarYear')[-1]}\
@@ -161,7 +163,18 @@ class Ticker(AbstractAPI):
         self.endpoint = endpoint
         return res
 
-    def get_ownership(self, incl_cur_q: bool=True, save_to_sql: bool=False,):
+    @classmethod
+    def download_transcripts(cls, ticker: str, 
+        year: int, quarter: Optional[int]=None):
+        """classmethod version of get_transcripts. Takes the same arguments
+        To use this, you must make sure the apikey is saved through:
+            ./FinancialModelingPrep/.config/config.json
+        Otherwise the api will not return
+        """
+        return cls(ticker, DEFAULT_CONFIG).get_transcripts(year=year, quarter=quarter)
+
+    def get_ownership(self, incl_cur_q: bool=True, 
+        save_to_sql: bool=False,):
         """interface for getting income/balance sheet/cash flow statements
         :param incl_cur_q: Include current Q or not
         """
@@ -188,3 +201,53 @@ class Ticker(AbstractAPI):
             return df
         else:
             raise TypeError("value returned from API is not a list")
+
+    @classmethod
+    def list_ownership(cls, ticker: str, incl_cur_q: bool=True):
+        """classmethod version of get_ownership"""
+        return cls(ticker=ticker, 
+            config=DEFAULT_CONFIG).get_ownership(incl_cur_q=incl_cur_q)
+
+    def __get_v4_info(self, url: str):
+        """template function for getting v4 info"""
+        endpoint = self.endpoint
+        self.endpoint = endpoint.replace("v3", "v4")
+        res = self._get_data(url=url, ticker=",".join(self.tickers))
+        self.endpoint = endpoint
+        return res
+
+    def get_peers(self):
+        """get the stock's peers"""
+        url = "stock_peers"
+        res = self.__get_v4_info(url=url)
+        return res
+
+    @classmethod
+    def list_peers(cls, ticker: Union[str, List[str]]):
+        """classmethod version of get_peers"""
+        res = cls(ticker=ticker, 
+            config=DEFAULT_CONFIG).get_peers()
+        return res
+
+    def get_core_info(self):
+        """get the stock's core information such as cik, exchange, industry"""
+        url = "company-core-information"
+        res = self.__get_v4_info(url=url)
+        return res
+
+    def get_profile(self):
+        """get company's profile information"""
+        url = urljoin("profile/", ",".join(self.tickers))
+        res = self._get_data(url=url)
+        if isinstance(res, list):
+            df = pd.concat([pd.Series(s).to_frame().T for s in res])
+            df = df.set_index(["symbol"])
+            return df
+        else: return res
+
+    @classmethod
+    def company_profile(cls, ticker: Union[str, List[str]]):
+        """classmethod version of get_profile"""
+        res = cls(ticker=ticker, 
+            config=DEFAULT_CONFIG).get_profile()
+        return res
